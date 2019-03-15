8078c3fa58df59400fdb867a6cf1119983c06583
Jorgeoliv <jorge10oliveira@gmail.com>
2019-03-15 10:41:26 +0000

    Adicionado a parte de enviar email

--- compositor/routes/index.js
+++ compositor/routes/index.js
@@ -2,6 +2,20 @@ var express = require('express');
 var router = express.Router();
 const axios = require('axios')
 const querystring = require('querystring')
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    // In Node, environment variables are available on process.env
+    host: process.env.MAILDEV_PORT_25_TCP_ADDR, // ex. 172.17.0.10
+    port: process.env.MAILDEV_PORT_25_TCP_PORT, // ex. 25
+    // We add this setting to tell nodemailer the host isn't secure during dev:
+    ignoreTLS: true
+    // service: 'gmail',
+    // auth: {
+    //        user: 'jorge10oliveira@gmail.com',
+    //        pass: 'password'
+    //    }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,6 +36,24 @@ router.get('/enviaEmail', function(req, res, next) {
       console.log("ERRO NA CONFIRMAÇÃO DO TOKEN:" + erroVerificacao)
     })
 });
+
+router.get('/teste', (req, res) => {
+
+  let mailOptions = {
+    from: 'jorge10oliveira@gmail.com', // sender address
+    to: 'jorge10oliveira@gmail.com', // list of receivers
+    subject: 'Subject of your email', // Subject line
+    html: '<p>Your html here</p>'// plain text body
+  }
+
+  // Now when your send an email, it will show up in the MailDev interface
+  transporter.sendMail(mailOptions, (error, info) => {
+    if(error)
+      console.log(error)
+    else
+      console.log(info)
+  })
+})
 
 // axios.get('http://localhost:3000/users/info?token=' + req.query.token)
 // .then(mail =>{
